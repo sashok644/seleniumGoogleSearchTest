1d1a3c912b1c68409a5749abaf5c9a4226f5c1ae
sashok644 <sashok644@yandex.ru>
2016-04-25 19:29:52 +0300

    fix

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -3,65 +3,49 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
+import static main.CustomConditions.*;
+import static main.CustomConditions.sizeOf;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.urlContains;
-
 /**
  * Created by 64 on 21.04.2016.
  */
 public class GoogleSearchTest {
 
     @Test
-    public void testSearch() {
+    public void testSearchAndFollowLink() {
 
         driver.get("http://google.com/ncr");
-        driver.findElement(By.name("q")).sendKeys("Selenium automates browsers" + Keys.ENTER);
-        wait.until(sizeOf(By.cssSelector(list), 10));
-        wait.until(textToBePresentInElementLocated(By.cssSelector(list + ":nth-child(1)"), "Selenium automates browsers"));
-
+        search("Selenium automates browsers");
+        wait.until(sizeOf(By.cssSelector(results), 10));
+        wait.until(textToBePresentInElementLocated(By.cssSelector(results + ":nth-child(1)"), "Selenium automates browsers"));
+        followNthLink(0);
+        wait.until(urlContains("http://www.seleniumhq.org/"));
     }
 
     @Test
     public void testFollowLink() {
 
         driver.get("http://google.com/ncr");
-        driver.findElement(By.name("q")).sendKeys("Selenium automates browsers" + Keys.ENTER);
-        wait.until(sizeOf(By.cssSelector(list), 10));
-        followNthLink(1);
+        search("Selenium automates browsers");
+        followNthLink(0);
         wait.until(urlContains("http://www.seleniumhq.org/"));
     }
 
     static WebDriver driver = new FirefoxDriver();
     static WebDriverWait wait = new WebDriverWait(driver, 10);
-    String list = ".srg>.g";
+    String results = ".srg>.g";
 
-    public void followNthLink(int index) {
-
-        driver.findElement(By.cssSelector(list + ":nth-child(" + index + ") .r")).click();
+    public void search(String queryText){
+        driver.findElement(By.name("q")).sendKeys(queryText + Keys.ENTER);
     }
 
-    public static ExpectedCondition<Boolean> sizeOf(final By elementsLocator, final int expectedSize) {
-        return new ExpectedCondition<Boolean>() {
-            private int listSize;
-            private List<WebElement> elements;
-
-            public Boolean apply(WebDriver driver) {
-                elements = driver.findElements(elementsLocator);
-                listSize = elements.size();
-                return listSize == expectedSize;
-            }
-
-            public String toString() {
-                return String.format("\nsize of list: %s\n to be: %s\n while actual size is: %s\n", elements, expectedSize, listSize);
-            }
-        };
+    public void followNthLink(int index) {
+        wait.until(minimumSizeOf(By.cssSelector(results), index + 1));
+        driver.findElements(By.cssSelector(results)).get(index).findElement(By.className("r")).click();
     }
 
     @AfterClass
---
+++ src/test/java/main/CustomConditions.java
@@ -0,0 +1,48 @@
+package main;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+import java.util.List;
+
+/**
+ * Created by MFM on 25.04.2016.
+ */
+public class CustomConditions {
+
+    public static ExpectedCondition<Boolean> sizeOf(final By elementsLocator, final int expectedSize) {
+        return new ExpectedCondition<Boolean>() {
+            private int listSize;
+            private List<WebElement> elements;
+
+            public Boolean apply(WebDriver driver) {
+                elements = driver.findElements(elementsLocator);
+                listSize = elements.size();
+                return listSize == expectedSize;
+            }
+
+            public String toString() {
+                return String.format("\nsize of results: %s\n to be: %s\n while actual size is: %s\n", elements, expectedSize, listSize);
+            }
+        };
+    }
+
+    public static ExpectedCondition<Boolean> minimumSizeOf(final By elementsLocator, final int expectedMinimumSize) {
+        return new ExpectedCondition<Boolean>() {
+            private int listSize;
+            private List<WebElement> elements;
+
+            public Boolean apply(WebDriver driver) {
+                elements = driver.findElements(elementsLocator);
+                listSize = elements.size();
+                return listSize >= expectedMinimumSize;
+            }
+
+            public String toString() {
+                return String.format("\nminimum size of results: %s\n to be: %s\n while actual size is: %s\n", elements, expectedMinimumSize, listSize);
+            }
+        };
+    }
+}
